513f0836b19858e8632610c13f7fa930a2b62b57
LaTanya Donaldson <ldonald234@gmail.com>
2018-06-30 11:06:31 +0300

    Wrap the variables in the template literal with the place holder expression

--- scripts/song-list.js
+++ scripts/song-list.js
@@ -2,10 +2,10 @@
    album.songs.forEach( (song, index) => {
      song.element = $(`
        <tr>
-         <td>index</td>
-         <td>song.title</td>
-         <td>song.duration</td>
-        </tr>
+        <td>${index}</td>
+        <td>${song.title}</td>
+        <td>${song.duration}</td>
+       </tr>
      `);
      $('#song-list').append(song.element);
    });
