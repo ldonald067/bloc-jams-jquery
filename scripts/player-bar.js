ef545e03e227203677d017ef9bb2c7e286085299
LaTanya Donaldson <ldonald234@gmail.com>
2018-07-04 12:41:26 -0700

    Calculate percentage to consider the different durations of all songs

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -27,6 +27,10 @@
   });
 
   setInterval( () => {
-    if (player.playState !== 'playing') { return; }
-  }, 1000)
+   if (player.playState !== 'playing') { return; }
+   const currentTime = player.getTime();
+   const duration = player.getDuration();
+   const percent = (currentTime / duration) * 100;
+   $('#time-control input').val(percent);
+ }, 1000);
 }
